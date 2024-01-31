6cb32a981a90778be6dda9721dad63bc36003e54
Nicole Honl <nicole.m.honl@gmail.com>
2024-01-31 17:30:14 -0600

    partially done with search form and profile view

--- forms.py
+++ forms.py
@@ -1,28 +1,75 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import (
+    StringField,
+    PasswordField,
+    TextAreaField,
+    IntegerField,
+    RadioField,
+    SelectField,
+)
 from wtforms.validators import DataRequired, Email, Length, Optional
+from mtgsdk import Card, Set, Type, Supertype, Subtype
+
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[Length(min=6)])
-    profile_image_url = StringField('(Optional) Profile Image URL', validators=[Optional(strip_whitespace=True)])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[Length(min=6)])
+    profile_image_url = StringField(
+        "(Optional) Profile Image URL", validators=[Optional(strip_whitespace=True)]
+    )
+
 
 class UserEditForm(FlaskForm):
     """Form for editing users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    profile_image_url = StringField('(Optional) Image URL', validators=[Optional(strip_whitespace=True)])
-    header_image_url = StringField('(Optional) Header Image URL', validators=[Optional(strip_whitespace=True)])
-    location = TextAreaField('(Optional) Location', validators=[Optional(strip_whitespace=True)])
-    bio = TextAreaField('(Optional) Tell us about yourself', validators=[Optional(strip_whitespace=True)])
-    password = PasswordField('Password', validators=[Length(min=6)])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    profile_image_url = StringField(
+        "(Optional) Image URL", validators=[Optional(strip_whitespace=True)]
+    )
+    header_image_url = StringField(
+        "(Optional) Header Image URL", validators=[Optional(strip_whitespace=True)]
+    )
+    location = TextAreaField(
+        "(Optional) Location", validators=[Optional(strip_whitespace=True)]
+    )
+    bio = TextAreaField(
+        "(Optional) Tell us about yourself",
+        validators=[Optional(strip_whitespace=True)],
+    )
+    password = PasswordField("Password", validators=[Length(min=6)])
+
 
 class LoginForm(FlaskForm):
     """Login form."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[Length(min=6)])
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[Length(min=6)])
+
+
+class SearchCardsForm(FlaskForm):
+    """Card search form"""
+
+    name = StringField("Name", validators=[Optional(strip_whitespace=True)])
+    set_name = StringField("Set Name", validators=[Optional(strip_whitespace=True)])
+    rarity = SelectField(
+        "Rarity",
+        choices=["Common", "Uncommon", "Rare", "Mythic rare"],
+        validators=[Optional()],
+    )
+    supertypes = SelectField(
+        "Supertypes", choices=Supertype.all(), validators=[Optional()]
+    )
+    types = SelectField("Types", choices=Type.all(), validators=[Optional()])
+    subtypes = SelectField("Subtypes", choices=Subtype.all(), validators=[Optional()])
+    cmc = IntegerField("Total Mana Cost", validators=[Optional(strip_whitespace=True)])
+    colors = RadioField(
+        "Colors",
+        choices=["White", "Black", "Blue", "Green", "Red"],
+        validators=[Optional()],
+    )
+    power = IntegerField("Power", validators=[Optional(strip_whitespace=True)])
+    toughness = IntegerField("Toughness", validators=[Optional(strip_whitespace=True)])
--- models.py
+++ models.py
@@ -5,132 +5,95 @@ from flask_sqlalchemy import SQLAlchemy
 bcrypt = Bcrypt()
 db = SQLAlchemy(session_options={"expire_on_commit": False})
 
+
 class User(db.Model):
-
     __tablename__ = "users"
-    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
     username = db.Column(db.Text, nullable=False)
-
     email = db.Column(db.Text, nullable=False)
-
     password = db.Column(db.Text, nullable=False)
-
-    header_image_url = db.Column(db.Text, default="/static/images/default-header-pic.png")
-
+    header_image_url = db.Column(
+        db.Text, default="/static/images/default-header-pic.png"
+    )
     profile_image_url = db.Column(db.Text, default="/static/images/default-pic.png")
-
     location = db.Column(db.Text)
-
     bio = db.Column(db.Text)
-
-    game_data = db.relationship('GameData')
-
-    cards_owned = db.relationship('CardsOwned')
-
-    card_wishlist = db.relationship('CardWishList')
-
-    decks = db.relationship('Decks')
+    game_data = db.relationship("GameData")
+    cards_owned = db.relationship("CardsOwned")
+    card_wishlist = db.relationship("CardWishList")
+    decks = db.relationship("Decks")
 
     @classmethod
     def signup(cls, username, email, password, profile_image_url):
-
-        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
-
+        hashed_pwd = bcrypt.generate_password_hash(password).decode("UTF-8")
         user = User(
             username=username,
             email=email,
             password=hashed_pwd,
             profile_image_url=profile_image_url,
         )
-
         db.session.add(user)
         return user
 
     @classmethod
     def authenticate(cls, username, password):
-
         user = cls.query.filter_by(username=username).first()
-
         if user:
             is_auth = bcrypt.check_password_hash(user.password, password)
             if is_auth:
                 return user
-
         return False
 
+
 class GameData(db.Model):
-
     __tablename__ = "game_data"
-
     game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'))
-
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="cascade"))
     winner_name = db.Column(db.Text)
-
     loser_name = db.Column(db.Text)
-
-    time_played = db.Column(db.Text)
-
+    sec_duration = db.Column(db.Integer)
     player1 = db.Column(db.Text, nullable=False)
-
     player2 = db.Column(db.Text, nullable=False)
-
     date_played = db.Column(db.Date, nullable=False)
 
+
 class CardsOwned(db.Model):
-
     __tablename__ = "cards_owned"
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'))
-
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="cascade"))
     card_id = db.Column(db.Integer, nullable=False)
+
 
 class CardWishList(db.Model):
-
     __tablename__ = "card_wish_list"
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'))
-
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="cascade"))
     card_id = db.Column(db.Integer, nullable=False)
+
 
 class Decks(db.Model):
-
     __tablename__ = "decks"
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'))
-
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="cascade"))
     cards = db.relationship("DeckCards", backref="decks")
+    deck_type = db.Column(db.Integer, db.ForeignKey("deck_types.id"))
 
-    deck_type = db.Column(db.Integer, db.ForeignKey('deck_types.id'))
 
 class DeckCards(db.Model):
-
     __tablename__ = "deck_cards"
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    deck_id = db.Column(db.Integer, db.ForeignKey('decks.id', ondelete='cascade'))
-
+    deck_id = db.Column(db.Integer, db.ForeignKey("decks.id", ondelete="cascade"))
     card_id = db.Column(db.Integer, nullable=False)
 
+
 class DeckTypes(db.Model):
-
     __tablename__ = "deck_types"
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
     deck_types = db.Column(db.Text, nullable=False)
 
+
 ############## CONNECT DB ##############
+
 
 def connect_db(app):
     db.app = app
